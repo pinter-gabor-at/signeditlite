5a63322a1e91f1cd4e16acbf641d3d751f6b05f7
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-16 15:02:23 +0100

    Bump versions.
    It is beta, because it was tested only with an alpha release of ModMenu.

--- src/main/java/eu/pintergabor/signeditlite/Global.java
+++ src/main/java/eu/pintergabor/signeditlite/Global.java
@@ -1,9 +1,11 @@
 package eu.pintergabor.signeditlite;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 
 public final class Global {
@@ -15,7 +17,7 @@ public final class Global {
 	/**
 	 * Used for logging and registration.
 	 */
-	public static final String MODID = "signeditlite";
+	public static final String MODID = "earlytobed";
 
 	/**
 	 * This logger is used to write text to the console and the log file.
@@ -28,8 +30,9 @@ public final class Global {
 	 *
 	 * @param path Name without {@link #MODID}.
 	 */
+	@Contract(pure = true)
 	@SuppressWarnings("unused")
-	public static String modName(String path) {
+	public static @NotNull String modName(String path) {
 		return MODID + ":" + path;
 	}
 
@@ -38,8 +41,9 @@ public final class Global {
 	 *
 	 * @param path Name without {@link #MODID}.
 	 */
+	@Contract("_ -> new")
 	@SuppressWarnings("unused")
-	public static ResourceLocation modId(String path) {
-		return ResourceLocation.fromNamespaceAndPath(MODID, path);
+	public static @NotNull Identifier modId(String path) {
+		return Identifier.fromNamespaceAndPath(MODID, path);
 	}
 }
